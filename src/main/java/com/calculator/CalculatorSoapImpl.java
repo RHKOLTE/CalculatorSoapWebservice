7b302859995521d9634c41e140147bd79455b52f
RHKOLTE <rhkolte@gmail.com>
2025-01-10 16:35:19 +0530

    Add files via upload
    First commit.

---
+++ src/main/java/com/calculator/CalculatorSoapImpl.java
@@ -0,0 +1,25 @@
+package com.calculator;
+
+public class CalculatorSoapImpl implements CalculatorSoap{
+
+	public int subtract(int intA, int intB) {
+		// TODO Auto-generated method stub
+		return intA-intB;
+	}
+
+	public int divide(int intA, int intB) {
+		// TODO Auto-generated method stub
+		return intA/intB;
+	}
+
+	public int add(int intA, int intB) {
+		// TODO Auto-generated method stub
+		return intA+intB;
+	}
+
+	public int multiply(int intA, int intB) {
+		// TODO Auto-generated method stub
+		return intA*intB;
+	}
+
+}
